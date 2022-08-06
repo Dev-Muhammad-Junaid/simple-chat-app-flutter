e4be102a992af9a451c57d050c6f953b322da01f
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2022-08-07 03:58:14 +0500

    Chat Ui with Expanded and Left right

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -28,86 +28,103 @@ class _ChatPageState extends State<ChatPage> {
           ),
         ],
       ),
-      body: ListView(
-        children:
-        [
-          Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+      body: Column(
+        children: [
+          Expanded(
+
+            child: ListView(children: [
+              Column(
                 children: [
-                  Text("This is the first Message"),
-                  SizedBox(
-                    height: 10,
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("This is the first Message"),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Image.network(
+                            'https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',
+                            height: 200,
+                          )
+                        ],
+                      ),
+                      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
                   ),
-                  Image.network(
-                    'https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',
-                    height: 200,
-                  )
-                ],
-              ),
-              margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-              decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(8)),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text("This is the first Message"),
-                  SizedBox(
-                    height: 10,
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("This is the first Message"),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Image.network(
+                            'https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',
+                            height: 200,
+                          )
+                        ],
+                      ),
+                      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
                   ),
-                  Image.network(
-                    'https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',
-                    height: 200,
-                  )
-                ],
-              ),
-              margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-              decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(8)),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text("This is the first Message"),
-                  SizedBox(
-                    height: 10,
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("This is the first Message"),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Image.network(
+                          'https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',
+                          height: 200,
+                        )
+                      ],
+                    ),
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(8)),
                   ),
-                  Image.network(
-                    'https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',
-                    height: 200,
-                  )
                 ],
               ),
-              margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-              decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(8)),
-            ),
-            Container(
-              height: 100,
-              child: Row(
-                children: [
-                  IconButton(onPressed: (){}, icon: Icon(Icons.send)),
-                  IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-                ],
-              ),
-              decoration: BoxDecoration(
-                color: Colors.grey[600]
-              ),
-            )
-          ],
-        ),
-      ]),
+            ]),
+          ),
+                Container(
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.add),
+                        color: Colors.black,
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.send),
+                          color: Colors.black),
+                    ],
+                  ),
+                  decoration: BoxDecoration(color: Colors.grey[600]),
+                )
+        ],
+      ),
     );
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,27 +8,16 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Signup"),
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: () { print("Button Clicked"); },),
-      //TODO: Add the text and button from the design
-      body: Column(
-        children: [
-          Text("Signup Now!️",style: loginstyle,),
-          Text("Welcome Back! \n We love you ❤️",style: loginstyle.copyWith(fontSize:20,fontWeight: FontWeight.w300),),
-          Image.network('https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',height: 300,),
-          Container(
-            height: 50,width: 50,
-            child: FlutterLogo(),
-            padding: EdgeInsets.all(5),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(10),
-            ),
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Signup Now!️",style: loginstyle,),
+            Text("Welcome Back! \n We love you ❤️",style: loginstyle.copyWith(fontSize:20,fontWeight: FontWeight.w300),textAlign: TextAlign.center,),
+            Image.network('https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',height: 200,),
+          ],
+        ),
       ),
     );
   }
