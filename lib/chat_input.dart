2e7de230a97387dafe366b2e7d90b150763e4365
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2022-08-13 03:09:55 +0500

    Chat Input is updated with Message box

--- lib/chat_input.dart
+++ lib/chat_input.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ChatInput extends StatelessWidget {
-  const ChatInput({Key? key}) : super(key: key);
+  ChatInput({Key? key}) : super(key: key);
+  final ChatEditingController = TextEditingController();
+  void onSendButtonPressed()
+  {
+    print("Message : "+ChatEditingController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
+
       height: 100,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -15,11 +21,26 @@ class ChatInput extends StatelessWidget {
             icon: Icon(Icons.add),
             color: Colors.black,
           ),
+          Expanded(
+            child: TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: 5,
+              textCapitalization: TextCapitalization.sentences,
+              minLines: 1,
+              controller: ChatEditingController,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Type your Messsage"
+              ),
+            ),
+          ),
           IconButton(
-              onPressed: () {}, icon: Icon(Icons.send), color: Colors.black),
+              onPressed: onSendButtonPressed, icon: Icon(Icons.send), color: Colors.black),
         ],
       ),
-      decoration: BoxDecoration(color: Colors.grey[600]),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(color: Colors.grey,spreadRadius: 5,
+        blurRadius: 10,
+        offset: Offset(0, 8),)]),
     );
   }
 }
