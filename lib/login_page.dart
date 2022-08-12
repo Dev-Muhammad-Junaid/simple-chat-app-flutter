be943d212be1b072afc532167f9b9b0a9680cd1e
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2022-08-13 02:53:59 +0500

    Form widget with Validation via Key

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,11 +1,27 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-
-
 class LoginPage extends StatelessWidget {
-  final loginstyle = TextStyle(fontWeight: FontWeight.bold,fontSize: 30);
+  final loginstyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 30);
+  final usernamecontroller = TextEditingController();
+  final passwordcontroller = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+
   void _loginuser() {
-    print("logged in");
+    if(_formkey.currentState!=null && _formkey.currentState!.validate())
+      {
+        print(usernamecontroller.text);
+        print(passwordcontroller.text);
+        print("logged in successfully");
+
+      }
+    else
+      {
+        print("Failed to login");
+      }
+
+
   }
 
   @override
@@ -15,16 +31,56 @@ class LoginPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text("Signup Now!️",style: loginstyle,textAlign: TextAlign.center,),
-              Text("Welcome Back! \n We love you ❤️",style: loginstyle.copyWith(fontSize:20,fontWeight: FontWeight.w300),textAlign: TextAlign.center,),
-              Image.network('https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',height: 200,),
-              ElevatedButton(onPressed: _loginuser, child:Text("Elevated")),
-              OutlinedButton(onPressed: (){}, child:FlutterLogo()),
-              TextButton(onPressed: (){}, child:Text("All rights reserved")),
+              Text(
+                "Signup Now!️",
+                style: loginstyle,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Welcome Back! \n We love you ❤️",
+                style: loginstyle.copyWith(
+                    fontSize: 20, fontWeight: FontWeight.w300),
+                textAlign: TextAlign.center,
+              ),
+              Image.network(
+                'https://www.kindpng.com/picc/m/62-622207_super-man-png-transparent-superman-png-png-download.png',
+                height: 200,
+              ),
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: usernamecontroller,
+                      validator: (value) {
+                        if (value != null && value.length < 5 && value.isNotEmpty) {
+                          return "Your Username should have atleast 5 characters";
+                        }
+                      },
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'Username'),
+                    ),
+                    SizedBox(height: 15,),
+                    TextFormField(
+                      controller: passwordcontroller,
+                      validator: (value) {
+                        if (value != null && value.length < 5 && value.isNotEmpty) {
+                          return "Your Password should have atleast 5 characters";
+                        }
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'password'),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(onPressed: _loginuser, child: Text("Login Now")),
+              OutlinedButton(onPressed: () {}, child: FlutterLogo(size: 25,)),
+              TextButton(onPressed: () {}, child: Text("All Rights Reserved")),
             ],
           ),
         ),
