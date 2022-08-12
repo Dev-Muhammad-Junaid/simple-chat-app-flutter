caf482d9512f4591d88d26dce08e6d56310b68b5
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2022-08-13 03:50:18 +0500

    Using NamedRoutes

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -5,8 +5,7 @@ import 'package:simple_chat_app/login_page.dart';
 import 'chat_input.dart';
 
 class ChatPage extends StatefulWidget {
-  final String username;
-  const ChatPage({Key? key,required this.username}) : super(key: key);
+  const ChatPage({Key? key}) : super(key: key);
 
   @override
   State<ChatPage> createState() => _ChatPageState();
@@ -15,12 +14,13 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
+    final username = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey[300],
         elevation: 1,
         title: Text(
-          widget.username,
+          username,
           style: TextStyle(fontSize: 20, color: Colors.black),
         ),
         actions: [
